ed50e1a43409c458169019510df7b2e8c94d4adc
Camilo G <cgarzonsa@gmail.com>
2026-01-02 18:04:34 -0500

    creacion sg

--- Laboratorio1/locals.tf
+++ Laboratorio1/locals.tf
@@ -2,7 +2,7 @@ locals {
   vpc_id = "vpc-093b20febcca86e89"
 }
 resource "aws_security_group" "sgtest" {
-  name        = "sg-test"
+  name        = "test-sg"
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = local.vpc_id
 
