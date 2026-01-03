7ce6fc997b8165b56d122d42d996741da0689b7c
Camilo G <cgarzonsa@gmail.com>
2026-01-02 21:32:33 -0500

    creacion backend

--- Laboratorio1/backend.tf
+++ Laboratorio1/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = module.s3_bucket.bucket          # Nombre del bucket donde se guardará el state
+    bucket = "tfstate-s3-bucket"              # Nombre del bucket donde se guardará el state
     key    = "laboratorio1/terraform.tfstate" # Ruta/nombre del archivo state dentro del bucket
     region = "us-east-1"                      # Región del bucket
     encrypt = true                            # Encriptar el state en S3
