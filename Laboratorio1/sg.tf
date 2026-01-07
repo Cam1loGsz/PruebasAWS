921cbcb0c82d4ed0eb9feb537516b084f7e40fda
Camilo G <cgarzonsa@gmail.com>
2026-01-07 17:57:58 -0500

    sg con modulo propio

--- Laboratorio1/sg.tf
+++ Laboratorio1/sg.tf
@@ -2,9 +2,9 @@ module "security_group" {
   source   = "git::https://github.com/Cam1loGsz/PruebasAWS.git//modules/SG?ref=main"
   for_each = local.security_groups
 
-  vpc_id         = lookup(local.security_groups, each.key, {})["vpc_id"]
-  sg_name        = lookup(local.security_groups, each.key, {})["sg_name"]
-  sg_description = lookup(local.security_groups, each.key, {})["sg_description"]
-  ingress_rules  = lookup(local.security_groups, each.key, {})["ingress_rules"]
-  egress_rules   = lookup(local.security_groups, each.key, {})["egress_rules"]
+  vpc_id        = lookup(local.security_groups, each.key, {})["vpc_id"]
+  name          = lookup(local.security_groups, each.key, {})["name"]
+  description   = lookup(local.security_groups, each.key, {})["description"]
+  ingress_rules = lookup(local.security_groups, each.key, {})["ingress_rules"]
+  egress_rules  = lookup(local.security_groups, each.key, {})["egress_rules"]
 }
