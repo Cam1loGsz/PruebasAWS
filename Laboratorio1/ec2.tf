536fe05fff46378e7de77036271f60bed7b6ca59
Camilo G <cgarzonsa@gmail.com>
2026-01-07 15:45:46 -0500

    creacion EC2

---
+++ Laboratorio1/ec2.tf
@@ -0,0 +1,9 @@
+module "ec2_instance" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
+  name   = "EC2-Test-Instance"
+  ami    = "ami-068c0051b15cdb816"
+  instance_type = "t3.micro"
+  key_name      = "ec2-key-pair"
+  user_data    = file("user_data.sh")
+  vpc_security_group_ids = [module.security_groups.this_security_group_id]
+}
---
+++ Laboratorio1/sg.tf
@@ -0,0 +1,28 @@
+module "security_groups" {
+    source = "git::https://github.com/terraform-aws-modules/terraform-aws-security-group.git"
+    name   = "ec2-security-group"
+    description = "Security group for EC2 instance"
+    vpc_id = local.vpc_id
+    ingress_rules = [
+        {
+            from_port   = 22
+            to_port     = 22
+            protocol    = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        },
+        {
+            from_port   = 80
+            to_port     = 80
+            protocol    = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+    ]
+    egress_rules = [
+        {
+            from_port   = 0
+            to_port     = 0
+            protocol    = "-1"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+    ]    
+}
