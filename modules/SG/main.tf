cb58ccf1db1ed8bbfa067a2494ba50b3603e6fa3
Camilo G <cgarzonsa@gmail.com>
2026-01-07 17:40:33 -0500

    Modulo SG

--- modules/SG/main.tf
+++ modules/SG/main.tf
@@ -3,3 +3,31 @@ resource "aws_security_group" "this" {
   description = var.description
   vpc_id      = var.vpc_id
 }
+
+resource "aws_security_group_ingress_rule" "ingress" {
+  type                     = "ingress"
+  security_group_id        = aws_security_group.this.id
+  from_port                = each.value.from_port
+  to_port                  = each.value.to_port
+  protocol                 = each.value.protocol
+  cidr_blocks              = lookup(each.value, "cidr_blocks", null)
+  source_security_group_id = lookup(each.value, "source_security_group_id", null)
+  self                     = lookup(each.value, "self", null)
+  prefix_list_id           = lookup(each.value, "prefix_list_id", null)
+  description              = lookup(each.value, "description", null)
+
+
+}
+resource "aws_security_group_egress_rule" "egress" {
+  type                     = "egress"
+  security_group_id        = aws_security_group.this.id
+  from_port                = each.value.from_port
+  to_port                  = each.value.to_port
+  protocol                 = each.value.protocol
+  cidr_blocks              = lookup(each.value, "cidr_blocks", null)
+  source_security_group_id = lookup(each.value, "source_security_group_id", null)
+  self                     = lookup(each.value, "self", null)
+  prefix_list_id           = lookup(each.value, "prefix_list_id", null)
+  description              = lookup(each.value, "description", null)
+
+}
--- modules/SG/variable.tf
+++ modules/SG/variable.tf
@@ -10,3 +10,33 @@ variable "vpc_id" {
     type        = string
     description = "VPC ID"
 }
+variable "ingress_rules" {
+  description = "Mapa de reglas ingress"
+  type = list(object({
+    from_port        = number
+    to_port          = number
+    protocol         = string
+    cidr_blocks      = optional(list(string))
+    security_groups  = optional(list(string))
+    prefix_list_id   = optional(list(string))
+    self             = optional(bool)
+    description      = optional(string)
+  }))
+  default = {}
+}
+
+variable "egress_rules" {
+  description = "Mapa de reglas egress"
+  type = list(object({
+    from_port        = number
+    to_port          = number
+    protocol         = string
+    cidr_blocks      = optional(list(string))
+    security_groups  = optional(list(string))
+    self             = optional(bool)
+    prefix_list_id   = optional(list(string))
+    description      = optional(string)
+  }))
+  default = {}
+  
+}
