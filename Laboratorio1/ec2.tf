f98d1da2a0d0022f74024176ef93fcd5f26d430f
Camilo G <cgarzonsa@gmail.com>
2026-01-07 16:36:41 -0500

    Eliminacion sg defaul

--- Laboratorio1/ec2.tf
+++ Laboratorio1/ec2.tf
@@ -1,6 +1,6 @@
 module "ec2_instance" {
   source                 = "git::https://github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
-  name                   = "EC2-Test-Instance"
+  name                   = "EC2-Instance"
   ami                    = "ami-068c0051b15cdb816"
   instance_type          = "t3.micro"
   key_name               = "ec2-key-pair"     # Asegúrate de tener este par de llaves creado en tu cuenta de AWS
@@ -8,4 +8,5 @@ module "ec2_instance" {
   vpc_security_group_ids = [module.security_group_ec2.security_group_id]
   subnet_id              = "subnet-04e7473716d36f167"
   iam_role_name          = aws_iam_role.ec2-test-role.name
+  create_security_group  = false
 }
