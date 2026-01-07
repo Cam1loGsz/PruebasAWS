9413c9aa33d36443699e6017139fab0dff65f669
Camilo G <cgarzonsa@gmail.com>
2026-01-07 18:08:10 -0500

    sg con modulo propio

--- Laboratorio1/locals.tf
+++ Laboratorio1/locals.tf
@@ -30,5 +30,32 @@ locals {
         }
       ]
     }
+    ec2_tests_sg2 = {
+      vpc_id         = "vpc-093b20febcca86e89"
+      sg_name        = "EC2-Tests-SG2"
+      sg_description = "Security Group for EC2 instance"
+      ingress_rules = [
+        {
+          from_port   = 22
+          to_port     = 22
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        },
+        {
+          from_port   = 80
+          to_port     = 80
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+      egress_rules = [
+        {
+          from_port   = 0
+          to_port     = 0
+          protocol    = "-1"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+    }
   }
 }
