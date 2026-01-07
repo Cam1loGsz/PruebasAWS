c5fa03fd787f311261580df8cfdff9ad97943005
Camilo G <cgarzonsa@gmail.com>
2026-01-07 16:14:30 -0500

    creacion EC2

--- Laboratorio1/ec2.tf
+++ Laboratorio1/ec2.tf
@@ -1,9 +1,10 @@
 module "ec2_instance" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
-  name   = "EC2-Test-Instance"
-  ami    = "ami-068c0051b15cdb816"
-  instance_type = "t3.micro"
-  key_name      = "ec2-key-pair"
-  user_data    = file("user_data.sh")
+  source                 = "git::https://github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
+  name                   = "EC2-Test-Instance"
+  ami                    = "ami-068c0051b15cdb816"
+  instance_type          = "t3.micro"
+  key_name               = "ec2-key-pair"
+  user_data              = file("user_data.sh")
   vpc_security_group_ids = [module.security_group_ec2.security_group_id]
+  subnet_id              = "subnet-04e7473716d36f167"
 }
