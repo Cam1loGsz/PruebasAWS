3586bb5aca772b085435fc39dbd7c5e4efebfc3c
Camilo G <cgarzonsa@gmail.com>
2026-01-07 18:00:37 -0500

    sg con modulo propio

--- modules/SG/main.tf
+++ modules/SG/main.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "this" {
   vpc_id      = var.vpc_id
 }
 
-resource "aws_security_group_ingress_rule" "ingress" {
+resource "aws_security_group_rule" "ingress" {
   type                     = "ingress"
   security_group_id        = aws_security_group.this.id
   from_port                = each.value.from_port
@@ -13,12 +13,12 @@ resource "aws_security_group_ingress_rule" "ingress" {
   cidr_blocks              = lookup(each.value, "cidr_blocks", null)
   source_security_group_id = lookup(each.value, "source_security_group_id", null)
   self                     = lookup(each.value, "self", null)
-  prefix_list_id           = lookup(each.value, "prefix_list_id", null)
+  prefix_list_ids           = lookup(each.value, "prefix_list_ids", null)
   description              = lookup(each.value, "description", null)
 
 
 }
-resource "aws_security_group_egress_rule" "egress" {
+resource "aws_security_group_rule" "egress" {
   type                     = "egress"
   security_group_id        = aws_security_group.this.id
   from_port                = each.value.from_port
@@ -27,7 +27,7 @@ resource "aws_security_group_egress_rule" "egress" {
   cidr_blocks              = lookup(each.value, "cidr_blocks", null)
   source_security_group_id = lookup(each.value, "source_security_group_id", null)
   self                     = lookup(each.value, "self", null)
-  prefix_list_id           = lookup(each.value, "prefix_list_id", null)
+  prefix_list_ids           = lookup(each.value, "prefix_list_ids", null)
   description              = lookup(each.value, "description", null)
 
 }
