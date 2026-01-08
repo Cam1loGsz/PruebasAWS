cacb6cdf8c5472b91bb2428b2e9385a2c2b3a11e
Camilo G <cgarzonsa@gmail.com>
2026-01-07 20:50:22 -0500

    agregar instance profile

--- Laboratorio1/ec2.tf
+++ Laboratorio1/ec2.tf
@@ -7,6 +7,6 @@ module "ec2_instance" {
   user_data              = file("user_data.sh")
   vpc_security_group_ids = [module.security_group["ec2_tests_sg"].security_group_id]
   subnet_id              = "subnet-04e7473716d36f167"
-  iam_role_name          = aws_iam_role.ec2-test-role.name
+  iam_instance_profile    = aws_iam_instance_profile.ec2_profile.name
   create_security_group  = false
 }
