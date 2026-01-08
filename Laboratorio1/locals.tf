59719df216d60b083a1a1a458e6f1e37d5bc6bb2
Camilo G <cgarzonsa@gmail.com>
2026-01-07 20:05:27 -0500

    eliminar ec2

--- Laboratorio1/locals.tf
+++ Laboratorio1/locals.tf
@@ -5,7 +5,7 @@ locals {
   security_groups = {
     ec2_tests_sg = {
       vpc_id      = "vpc-093b20febcca86e89"
-      name        = "EC2-Tests-SG"
+      name        = "ec2-tests-sg"
       description = "Security Group for EC2 instance"
       ingress_rules = [
         {
@@ -19,6 +19,12 @@ locals {
           to_port     = 80
           protocol    = "tcp"
           cidr_blocks = ["0.0.0.0/0"]
+        },
+        {
+          from_port   = 443
+          to_port     = 443
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
         }
       ]
       egress_rules = [
@@ -30,6 +36,6 @@ locals {
         }
       ]
     }
-    
+
   }
 }
