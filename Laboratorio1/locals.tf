83fb4ab9c984d662211592344624cd79acadcbdc
Camilo G <cgarzonsa@gmail.com>
2026-01-07 19:53:19 -0500

    sg test 5

--- Laboratorio1/locals.tf
+++ Laboratorio1/locals.tf
@@ -4,7 +4,7 @@ locals {
 locals {
   security_groups = {
     ec2_tests_sg = {
-      vpc_id         = "vpc-093b20febcca86e89"
+      vpc_id      = "vpc-093b20febcca86e89"
       name        = "EC2-Tests-SG"
       description = "Security Group for EC2 instance"
       ingress_rules = [
@@ -30,32 +30,6 @@ locals {
         }
       ]
     }
-    ec2_tests_sg2 = {
-      vpc_id         = "vpc-093b20febcca86e89"
-      name        = "EC2-Tests-SG2"
-      description = "Security Group for EC2 instance"
-      ingress_rules = [
-        {
-          from_port   = 22
-          to_port     = 22
-          protocol    = "tcp"
-          cidr_blocks = ["0.0.0.0/0"]
-        },
-        {
-          from_port   = 80
-          to_port     = 80
-          protocol    = "tcp"
-          cidr_blocks = ["0.0.0.0/0"]
-        }
-      ]
-      egress_rules = [
-        {
-          from_port   = 0
-          to_port     = 0
-          protocol    = "-1"
-          cidr_blocks = ["0.0.0.0/0"]
-        }
-      ]
-    }
+    
   }
 }
