24d89ba97b9aa9cfd28e01ca93b7400f52bdcf5d
Camilo G <cgarzonsa@gmail.com>
2026-01-07 20:30:20 -0500

    regla a mi ip

--- Laboratorio1/locals.tf
+++ Laboratorio1/locals.tf
@@ -12,7 +12,7 @@ locals {
           from_port   = 22
           to_port     = 22
           protocol    = "tcp"
-          cidr_blocks = ["186.155.140.81/32"]
+          cidr_blocks = ["0.0.0.0/0"]
         },
         {
           from_port   = 80
