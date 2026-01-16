3a3d5295b3a8d04f528a43d3af590cc09a358382
Camilo G <cgarzonsa@gmail.com>
2026-01-16 12:33:03 -0500

    nada

--- Laboratorio1/locals.tf
+++ Laboratorio1/locals.tf
@@ -1,96 +1,96 @@
-# locals {
-#   vpc_id = "vpc-093b20febcca86e89"
-#   subnet_ids = [
-#    "subnet-04e7473716d36f167",
-#    "subnet-07122f4adb1d76421"
-#   ]
-# }
-# locals {
-#   security_groups = {
-#     ec2_tests_sg = {
-#       vpc_id      = "vpc-093b20febcca86e89"
-#       name        = "ec2-sg"
-#       description = "Security Group for EC2 instance"
-#       ingress_rules = [
-#         {
-#           from_port   = 22
-#           to_port     = 22
-#           protocol    = "tcp"
-#           cidr_blocks = ["0.0.0.0/0"]
-#         },
-#         {
-#           from_port   = 80
-#           to_port     = 80
-#           protocol    = "tcp"
-#           security_groups = ["sg-064402de8a9bbbef6","sg-0496b6c3ab65eab62"]
-#         },
-#         {
-#           from_port   = 443
-#           to_port     = 443
-#           protocol    = "tcp"
-#           cidr_blocks = ["0.0.0.0/0"]
-#         }
-#       ]
-#       egress_rules = [
-#         {
-#           from_port   = 0
-#           to_port     = 0
-#           protocol    = "-1"
-#           cidr_blocks = ["0.0.0.0/0"]
-#         }
-#       ]
-#     }
-#     alb_tests_sg = {
-#       vpc_id      = "vpc-093b20febcca86e89"
-#       name        = "alb-sg"
-#       description = "Security Group for alb"
-#       ingress_rules = [
-#         {
-#           from_port   = 80
-#           to_port     = 80
-#           protocol    = "tcp"
-#           cidr_blocks = ["0.0.0.0/0"]
-#         },
-#         {
-#           from_port   = 443
-#           to_port     = 443
-#           protocol    = "tcp"
-#           cidr_blocks = ["0.0.0.0/0"]
-#         }
-#       ]
-#       egress_rules = [
-#         {
-#           from_port   = 0
-#           to_port     = 0
-#           protocol    = "-1"
-#           cidr_blocks = ["0.0.0.0/0"]
-#         }
-#       ]
-#     }
-#     nlb_tests_sg = {
-#       vpc_id      = "vpc-093b20febcca86e89"
-#       name        = "nlb-sg"
-#       description = "Security Group for alb"
-#       ingress_rules = [
-#         {
-#           from_port   = 80
-#           to_port     = 80
-#           protocol    = "tcp"
-#           cidr_blocks = ["0.0.0.0/0"]
-#         }
-#       ]
-#       egress_rules = [
-#         {
-#           from_port   = 0
-#           to_port     = 0
-#           protocol    = "-1"
-#           cidr_blocks = ["0.0.0.0/0"]
-#         }
-#       ]
-#     }
+locals {
+  vpc_id = "vpc-093b20febcca86e89"
+  subnet_ids = [
+   "subnet-04e7473716d36f167",
+   "subnet-07122f4adb1d76421"
+  ]
+}
+locals {
+  security_groups = {
+    ec2_tests_sg = {
+      vpc_id      = "vpc-093b20febcca86e89"
+      name        = "ec2-sg"
+      description = "Security Group for EC2 instance"
+      ingress_rules = [
+        {
+          from_port   = 22
+          to_port     = 22
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        },
+        {
+          from_port   = 80
+          to_port     = 80
+          protocol    = "tcp"
+          security_groups = ["sg-064402de8a9bbbef6","sg-0496b6c3ab65eab62"]
+        },
+        {
+          from_port   = 443
+          to_port     = 443
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+      egress_rules = [
+        {
+          from_port   = 0
+          to_port     = 0
+          protocol    = "-1"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+    }
+    alb_tests_sg = {
+      vpc_id      = "vpc-093b20febcca86e89"
+      name        = "alb-sg"
+      description = "Security Group for alb"
+      ingress_rules = [
+        {
+          from_port   = 80
+          to_port     = 80
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        },
+        {
+          from_port   = 443
+          to_port     = 443
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+      egress_rules = [
+        {
+          from_port   = 0
+          to_port     = 0
+          protocol    = "-1"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+    }
+    nlb_tests_sg = {
+      vpc_id      = "vpc-093b20febcca86e89"
+      name        = "nlb-sg"
+      description = "Security Group for alb"
+      ingress_rules = [
+        {
+          from_port   = 80
+          to_port     = 80
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+      egress_rules = [
+        {
+          from_port   = 0
+          to_port     = 0
+          protocol    = "-1"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+    }
 
-#   }
-# }
+  }
+}
 ##### EBS VOLUME #####
 # resource "aws_ebs_volume" "volume2" {
 #   availability_zone = "us-east-1a"
